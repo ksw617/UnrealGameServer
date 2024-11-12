69d6cd41658b888a32e2cdc76e7dbb4e442962af
lecture <ksw617@gmail.com>
2024-11-12 20:08:59 +0900

    update

--- Protoc/bin/PacketGenerator.bat
+++ Protoc/bin/PacketGenerator.bat
@@ -2,8 +2,6 @@ pushd %~dp0
 
 protoc.exe --proto_path=./ --cpp_out=./ ./Protocol.proto
 
-protoc.exe -I=./ --csharp_out=./ ./Protocol.proto 
-
 IF ERRORLEVEL 1 PAUSE
 
 XCOPY /Y Protocol.pb.h "../../GameClient"
@@ -12,6 +10,3 @@ XCOPY /Y Protocol.pb.cc "../../GameClient"
 
 XCOPY /Y Protocol.pb.h "../../GameServer"
 XCOPY /Y Protocol.pb.cc "../../GameServer"
-
-
-XCOPY /Y Protocol.cs "../../UnityClient/Packet"
